4d23d7339adb00ce5f3a73b9f4593e4ff0a2e02a
Jan Witt <wittjan03@gmail.com>
2026-02-19 17:44:56 +0100

    simple demo with tts response from llm. new directory with the llm content.

--- llm/nlp_llm.py
+++ llm/nlp_llm.py
@@ -1,33 +1,35 @@
-import ollama
 import json
-import requests
+
+import ollama
 import rclpy
+import requests
 from rclpy.node import Node
 from std_msgs.msg import String
+
 
 class LLMNode(Node):
     def __init__(self):
         super().__init__("llm_node")
-        self.publisher_ = self.create_publisher(String, '/tts_text', 10)
+        self.publisher_ = self.create_publisher(String, "/tts_text", 10)
         self.get_logger().info("LLM Node started.")
 
     def run(self):
         while True:
-            u_input = input('\nYou: ')
-            if u_input.lower() == 'exit':
+            u_input = input("\nYou: ")
+            if u_input.lower() == "exit":
                 break
 
             response = ollama.chat(
-                model='nlp_llama',
+                model="nlp_llama",
                 messages=[
-                    {'role': 'user', 'content': u_input},
-                ]
+                    {"role": "user", "content": u_input},
+                ],
             )
 
-            reply = response['message']['content']
+            reply = response["message"]["content"]
 
             parsed = json.loads(reply)
-            answer = parsed['response']
+            answer = parsed["response"]
 
             print(answer)
             print(f"\nLLM: {reply}")
@@ -35,7 +37,6 @@ class LLMNode(Node):
             msg = String()
             msg.data = answer
             self.publisher_.publish(msg)
-
 
 
 def main():
@@ -49,5 +50,6 @@ def main():
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
